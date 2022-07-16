bf836a95b4934a8664579ad77d6a14e8697461b5
daneliyan <105802998+daneliyan@users.noreply.github.com>
2022-07-16 04:12:45 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,49 @@
+let header = document.querySelector('.header');
+
+window.onscroll = () => {
+    if (window.scrollY > 0) {
+        header.classList.add('active');
+    } else {
+        header.classList.remove('active');
+    }
+}
+
+(function () {
+    const burger = document?.querySelector('[data-burger]');
+    const menu = document?.querySelector('[data-menu]');
+    const menuItems = document?.querySelectorAll('[data-menu-item]');
+    const overlay = document?.querySelector('[data-menu-overlay]');
+
+    burger?.addEventListener('click', (e) => {
+        burger?.classList.toggle('burger--active');
+        menu?.classList.toggle('menu--active');
+
+        if (menu?.classList.contains('menu--active')) {
+            burger?.setAttribute('aria-expanded', 'true');
+            burger?.setAttribute('aria-label', 'Закрыть меню');
+            disableScroll();
+        } else {
+            burger?.setAttribute('aria-expanded', 'false');
+            burger?.setAttribute('aria-label', 'Открыть меню');
+            enableScroll();
+        }
+    });
+
+    overlay?.addEventListener('click', () => {
+        burger?.setAttribute('aria-expanded', 'false');
+        burger?.setAttribute('aria-label', 'Открыть меню');
+        burger.classList.remove('burger--active');
+        menu.classList.remove('menu--active');
+        enableScroll();
+    });
+
+    menuItems?.forEach(el => {
+        el.addEventListener('click', () => {
+            burger?.setAttribute('aria-expanded', 'false');
+            burger?.setAttribute('aria-label', 'Открыть меню');
+            burger.classList.remove('burger--active');
+            menu.classList.remove('menu--active');
+            enableScroll();
+        });
+    });
+})();
